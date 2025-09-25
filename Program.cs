b06cec52ed73216798d61a8677e6ed99cd47eb0a
Frededrick Agyemang <6045048@mborijnland.nl>
2025-09-25 12:10:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using Bogus;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("nl-NL");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("nl-NL");
+
+Randomizer.Seed = new Random(1234);
+
+string[] diensten = { "Ambulance", "Brandweer", "Politie" };
+string[] types = { "Verkeersongeval", "Brand", "Spoedrit", "Inbraak", "Reanimatie" };
+
+// Create a Faker instance for generating fake reports in Dutch
+var meldingFaker = new Faker<Melding>("nl")
+    .RuleFor(m => m.Hulpdienst, f => f.PickRandom(diensten))
+    .RuleFor(m => m.Locatie, f => f.Address.StreetAddress() + ", " + f.Address.City())
+    .RuleFor(m => m.Type, f => f.PickRandom(types))
+    .RuleFor(m => m.Tijdstip, f => f.Date.Recent(1));
+
+// Generate 500 fake reports
+var meldingen = meldingFaker.Generate(500);
+
+
+// Display each generated report with the date, service, type, and location
+foreach (var m in meldingen)
+{
+    Console.WriteLine($"{m.Tijdstip:G}: {m.Hulpdienst} -> {m.Type} op {m.Locatie}");
+}
+
+Console.WriteLine("\nDruk op een toets om af te sluiten...");
+Console.ReadKey();
+
+// Define the class after the code
+public class Melding
+{
+    public string Hulpdienst { get; set; }
+    public string Locatie { get; set; }
+    public string Type { get; set; }
+    public DateTime Tijdstip { get; set; }
+}
